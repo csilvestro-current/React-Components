4e72e555320300d0bab93a43bfe6e48f2081e5c8
csilvestro <crystal@csilvestro.com>
2022-01-10 22:10:23 -0500

    starting to make search component.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ReactComponents
+# React Components
+This repo is created for indiviual component pieces.
+
+
+Install
+---
+
+`npm install`
+
+
+Usage
+---
+
+`npm start`
+
+Prototype
+---
+<img src="slack.png" width="60%" height="60%" />
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+//React Router
+// import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import HeaderInsta from './components/HeaderInsta';
+// import Dashboard from './pages/Dashboard';
+// import Reports from './pages/Reports';
+// import Income from './pages/Income';
+// import Spending from './pages/Spending'; 
+// import Savings from './pages/Savings';
+// import Settings from './pages/Settings';
+
+function App() {
+  return (
+      <div style={styles.container}>
+        <Nav />
+        {/* <section style={styles.header}> */}
+          <Header />    
+          {/* <Routes>
+            <Route exact path='/' element={<Dashboard />} /> 
+            <Route path='/Dashboard' element={<Dashboard />} />
+            <Route path='/Reports' element={<Reports />} />
+            <Route path='/Income' element={<Income />} />
+            <Route path='/Spending' element={<Spending />} />
+            <Route path='/Savings' element={<Savings />} />
+            <Route path='/Settings' element={<Settings />} /> 
+          </Routes> */}
+        {/* </section> */}
+      </div>
+    );
+  }
+export default App;
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  main: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+}
